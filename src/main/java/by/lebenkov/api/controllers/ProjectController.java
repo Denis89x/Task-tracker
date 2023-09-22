52494643dc17deb06827bc059b309ffa6778ef2d
Denis Lebenkov <sabzz1ro3@mail.ru>
2023-09-22 15:56:38 +0300

    Added handle exception

--- src/main/java/by/lebenkov/api/controllers/ProjectController.java
+++ src/main/java/by/lebenkov/api/controllers/ProjectController.java
@@ -1,4 +1,39 @@
 package by.lebenkov.api.controllers;
 
+import by.lebenkov.api.dto.ProjectDto;
+import by.lebenkov.api.exceptions.BadRequestException;
+import by.lebenkov.api.factories.ProjectDtoFactory;
+import by.lebenkov.store.entities.ProjectEntity;
+import by.lebenkov.store.repositories.ProjectRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Transactional
+@RestController
 public class ProjectController {
+
+    ProjectDtoFactory projectDtoFactory;
+
+    ProjectRepository projectRepository;
+
+    public static final String CREATE_PROJECT = "/api/projects";
+
+    @PostMapping(CREATE_PROJECT)
+    public ProjectDto createProject(@RequestParam String name) {
+        projectRepository.
+                findByName(name)
+                .ifPresent(project -> {
+                    throw new BadRequestException(String.format("Project \"%s\" already exists", name));
+                });
+
+        //TODO: uncomment and insert entity in method
+        return null;
+    }
 }
